e953cd515182b05bfe363a3f12118a31a43d792a
Bruuno96 <brunotkd96@gmail.com>
2022-05-03 15:33:18 -0300

    Exibindo mensagem com ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
 
   transferir($event) {
     console.log($event);
-    this.transferencias.push($event);
+    const transferencia = {...$event, data : new Date()};
+    this.transferencias.push(transferencia);
   }
 }
